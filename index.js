aab7ebdb830324411cfe8f5a1cb48f103bf95715
Fe. Pastore <fesp.dotto@gmail.com>
2022-01-26 10:16:45 -0300

    Validando CPF antes do salvamento

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const CPF = require('cpf');
 // fs é uma Biblioteca nativa do Node.js onde nela existe diversas funções já utilizáveis 
 const fs = require('fs');
-fs.writeFileSync
+
 
 //imprime uma mensagem de boas vindas   
 console.log("Seja bem vindo ao cadastrador de CPFs");
@@ -11,13 +11,13 @@ let cpf = process.argv[2]
      
 
 //2 - Validar o CPF...
-let cpfValido = CPF.isValid(cpf)    
+let cpfValido = CPF.isValid(cpf);    
 
 //3 - Caso a informação seja um CPF válido, salvar no arquivo dados.txt
 //        caso contrário, exibir mensagem de erro:
 if(cpfValido){
     console.log('CPF Válido! Salvando.')
-    fs.writeFileSync('dados.txt','cpf:' + cpf + "\n", {flag:'a'});
+    fs.writeFileSync('dados.txt', cpf + "\n", {flag:'a'});
 } else {
     console.log('CPF Inválido, por gentileza digitar um CPF válido')
 }
